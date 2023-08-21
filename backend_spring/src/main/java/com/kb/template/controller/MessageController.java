53e0de9d2fe54c279771b091bd8c90acab170892
Umut Arslan <umut.arslan@klosebrothers.de>
2023-08-21 11:19:13 +0200

    change Controller to RestController

--- backend_spring/src/main/java/com/kb/template/controller/MessageController.java
+++ backend_spring/src/main/java/com/kb/template/controller/MessageController.java
@@ -7,14 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/push")
 public class MessageController {
     @Autowired
     private MessageService messageService;
 
-    @GetMapping("/publickey")
+    @GetMapping("/publicKey")
     public @Nonnull String getPublicKey() {
         return messageService.getPublicKey();
     }
